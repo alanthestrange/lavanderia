bdc96df5377e6dbe5ac6ea8784bcf3a4f5ec4165
alanc <alanc@Alan-VivobookPro>
2023-02-27 19:08:14 +0100

    seconda versione

--- lavanderiagettoni/src/it/edu/iisvolta/Main.java
+++ lavanderiagettoni/src/it/edu/iisvolta/Main.java
@@ -1,12 +1,135 @@
 package it.edu.iisvolta;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		// inizializzo le lavatrici
+				Scanner s=new Scanner(System.in);
+				String scelta, lav;
+				int numLavatrice=1;
+				
+				lavatrice l1=new lavatrice(8);
+				lavatrice l2=new lavatrice(10);
+				lavatrice l3=new lavatrice(14);
+				
+				lavatrice lavatriceSelezionata=l1;
+				
+				do {
+					menu();
+					Console.setCursorPosition(1, 70);
+					System.out.print("Lavatrice selezionata: "+numLavatrice);
+					Console.setCursorPosition(2, 70);
+					System.out.print("  Capacità: "+lavatriceSelezionata.getCapacità());
+					Console.setCursorPosition(3, 70);
+					System.out.print("  Stato: "+lavatriceSelezionata.getStato());
+					Console.setCursorPosition(4, 70);
+					System.out.print("  Livello acqua: "+lavatriceSelezionata.getLivelloacqua());
+					Console.setCursorPosition(5, 70);
+					System.out.print("  Temperatura: "+lavatriceSelezionata.getTemperatura());
+					Console.setCursorPosition(6, 70);
+					System.out.print("  Velocità rotazione: "+lavatriceSelezionata.getVelocitàrotazione());
+					Console.setCursorPosition(7, 70);
+					System.out.print("  Detersivo: "+lavatriceSelezionata.detersivoAggiunto());
+					Console.setCursorPosition(8, 70);
+					System.out.print("  Costo: "+lavatriceSelezionata.getCosto());
+					Console.setCursorPosition(9, 70);
+					System.out.print("  Credito: "+lavatriceSelezionata.getCredito());
+					
+					Console.setCursorPosition(15, 1);
+					
+					System.out.print("Inserisci la tua scelta: ");
+					scelta=s.nextLine();
+					switch (scelta) {
+					case "1":		//scelta lavatrice
+						System.out.print("Quale lavatrice vuoi scegliere (1-3)? ");
+						lav=s.nextLine();
+						switch(lav) {
+						case "1":
+							lavatriceSelezionata=l1;
+							numLavatrice=1;
+							break;
+						case "2":
+							lavatriceSelezionata=l2;
+							numLavatrice=2;
+							break;
+						case "3":
+							lavatriceSelezionata=l3;
+							numLavatrice=3;
+							break;
+						default:
+							System.out.println("Lavatrice non valida!");
+							break;
+						}
+						break;
+						
+					case "2": 	//aggiungi acqua 
+						System.out.print("Quanta acqua vuoi aggiungere? ");
+						int litri=Integer.parseInt(s.nextLine());
+						lavatriceSelezionata.aggiungiAcqua(litri);
+						break;
+						
+					case "3": 	//imposta temperatura 
+						System.out.print("Che temperatura vuoi impostare? ");
+						int temp=Integer.parseInt(s.nextLine());
+						lavatriceSelezionata.setTemperatura(temp);
+						break;
+						
+					case "4": 	//imposta numero di giri 
+						System.out.print("Inserisci il numero di giri: ");
+						int giri=Integer.parseInt(s.nextLine());
+						lavatriceSelezionata.setVelocitàRotazione(giri);;
+						break;
+						
+					case "5": 	//aggiungi detersivo 				
+						lavatriceSelezionata.aggiungiDetersivo();
+						break;
+						
+					case "6": 	//aggiungi moneta 	
+						System.out.print("Che moneta vuoi aggiungere? ");
+						float moneta=Float.parseFloat(s.nextLine());
+						lavatriceSelezionata.aggiungiMoneta(moneta);
+						break;
+						
+					case "7": 	//avvio 				
+						lavatriceSelezionata.start();
+						break;
+						
+					case "8": 	//stop 				
+						lavatriceSelezionata.stop();
+						break;
+						
+					case "9":
+						lavatriceSelezionata.svuota();
+						break;
+						
+					}
+					System.out.println();
+					System.out.println("Premi Enter per continuare...");
+					s.nextLine();
+					
+					
+				} while (!scelta.equals("9"));
+				s.close();
+				
 
-		try {}
-		catch (Exception ex)
-		{ System.out.println("Si è verificato un errore: " + ex.getMessage()); }
+			}
+			
+			private static void menu() {
+				Console.clear();
+				System.out.println("**** LAVANDERIA A GETTONI ****");
+				System.out.println();
+				System.out.println("1) Scegli lavatrice");
+				System.out.println("2) Aggiungi acqua");
+				System.out.println("3) Imposta temperatura");
+				System.out.println("4) Imposta numero giri");
+				System.out.println("5) Aggiungi detersivo");
+				System.out.println("6) Aggiungi moneta");		
+				System.out.println("7) Avvio");
+				System.out.println("8) Stop");
+				System.out.println("9) Svuota");
+				System.out.println("0) Esci");
 	}
-
 }
--- lavanderiagettoni/src/it/edu/iisvolta/lavatrice.java
+++ lavanderiagettoni/src/it/edu/iisvolta/lavatrice.java
@@ -6,27 +6,24 @@ import java.time.LocalTime;
 
 public class lavatrice {
 	private int capacità, livelloacqua, temperatura, velocitàrotazione, stato;
-	LocalTime start;
+	LocalTime dataOraAvvio;
 	private boolean detersivo;
 	private float credito;
 	
-	public void lavatrice(int capacità) throws Exception{
-		if (capacità != 8 || capacità != 10 || capacità != 14)
+	public lavatrice(int capacità) throws Exception{
+		super();
+		if (capacità != 8 && capacità != 10 && capacità != 14)
 		{ System.out.println("Le lavatrici disponibili sono 8, 10 e 14 chilogrammi"); }
 		else {
 		this.capacità = capacità;
-		this.livelloacqua=0;
-		this.temperatura=0;
-		this.velocitàrotazione=0;
-		this.stato=0;
-		this.credito=0;
-		this.detersivo=false;
-		throw new Exception("Capacità non disponibile");
+		inizializza();
 		}
 	}
 	
 	public void aggiungiAcqua(int litri) {
-		if (livelloacqua+litri > capacità || litri > 6)
+		if (litri<=0)
+		{ System.out.println("Livello acqua non valido"); }
+		else if (livelloacqua+litri > capacità || litri > 6)
 		{ System.out.println("Quantità di acqua maggiore della capacità della lavatrice selezionata o maggiore di 6 litri"); }
 		else
 		{ livelloacqua=livelloacqua+litri; }
@@ -57,93 +54,103 @@ public class lavatrice {
 		if (moneta != 0.5 || moneta != 1 || moneta != 2)
 		{ System.out.println("La lavatrice accetta solo monete da 0.50, 1 o 2 euro"); }
 		else
-		{ credito=credito+moneta; }
+		{ credito+=moneta; }
+	}
+	
+	public float getCredito() {
+		return credito;
+	}
+
+	public float getCosto() {
+		switch(capacità) {
+			case 8:
+				return 4.50f;
+			case 10:
+				return 5.50f;
+			case 14:
+				return 6.50f;
+			default:
+				return 0;
+		}
 	}
 	
 	public void start() {
-		if (livelloacqua<4)
+		if (credito<getCosto())
+		{ System.out.println("Il credito non è sufficiente"); }
+		else if (livelloacqua<4)
 		{ System.out.println("Livello di acqua minore di 4 litri, impossibile avviare il lavaggio"); }
+		else if(temperatura == 0)
+		{ System.out.println("Devi impostare prima la temperatura"); }
+		else if(velocitàrotazione == 0)
+		{ System.out.println("Devi impostare prima la centrifuga"); }
+		else if (detersivo=false)
+		{ System.out.println("Devi aggiungere il detersivo per iniziare il lavaggio"); }
+		else if (stato!=0)
+		{ System.out.println("La lavatrice non è libera"); }
 		else
-		{
-			if(temperatura != 40 || temperatura != 60 || temperatura != 90)
-			{ System.out.println("Devi impostare prima la temperatura"); }
-			else
-			{
-				if (velocitàrotazione != 600 || velocitàrotazione != 800 || velocitàrotazione != 1000)
-				{ System.out.println("Devi impostare prima la centrifuga"); }
-				else
-				{
-					if (detersivo=false)
-					{ System.out.println("Devi aggiungere il detersivo per iniziare il lavaggio"); }
-					else
-					{
-						stato=1;
-						start=LocalTime.now();
-						
-						switch (capacità)
-						{
-							case 8:
-								if (credito<4.5)
-								{ System.out.println("Il credito non è sufficiente"); }
-								else
-								{ credito=(float) (credito-4.5); }
-								break;
-							case 10:
-								if (credito<5.50)
-								{ System.out.println("Il credito non è sufficiente"); }
-								else
-								{ credito=(float) (credito-5.50); }
-								break;
-							case 14:
-								if (credito<6.50)
-								{ System.out.println("Il credito non è sufficiente"); }
-								else
-								{ credito=(float) (credito-6.50); }
-								break;
-							default:
-								break;
-						}
-					}
-				}
-			}
-		}	
+		{ stato=1;
+		dataOraAvvio=LocalTime.now();
+		}
 	}
 	
 	public void stop() {
 		if (stato==1)
 		{ stato=2; }
+		else
+		{ System.out.println("Lavatrice non in funzione"); }
 	}
 	
 	String getStato() {
-		switch (this.stato)
+		LocalTime lt=LocalTime.now();
+		if (stato==1 && SECONDS.between(dataOraAvvio, lt)>60)
+		{ stato = 2; }
+		switch (stato)
 		{
 			case 0:
-				System.out.println("Lavatrice pronta per il lavaggio");
-				break;
+				return "Lavatrice pronta per il lavaggio";
 			case 1:
-				System.out.println("Lavaggio in corso");
-				break;
+				return "Lavaggio in corso";
 			case 2:
-				System.out.println("Lavaggio terminato");
-				break;
+				return "Lavaggio terminato";
 			default:
-				break;				
+				return "";
 		}
-		return;
 	}	
 	
 	public void svuota() {
 		if (stato==2)
 		{ stato=0; }
 		else
-		{ }
+		{ System.out.println("");}
+	}
+
+	private void inizializza() {
+		livelloacqua=0;
+		temperatura=0;
+		velocitàrotazione=0;
+		stato=0;
+		credito=0;
+		detersivo=false;
 	}
 	
-	int getLivelloAcqua() {
-		System.out.println("Livello acqua " + livelloacqua);
-		
+	public int getCapacità() {
+		return capacità;
+	}
+
+	public int getLivelloacqua() {
+		return livelloacqua;
+	}
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public int getVelocitàrotazione() {
+		return velocitàrotazione;
 	}
 	
-	
+	public boolean detersivoAggiunto() {
+		return detersivo;
+	}
 	
 }
